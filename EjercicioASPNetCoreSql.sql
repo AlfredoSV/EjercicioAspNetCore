3f2855615aa6e967c41700735ca0eab360d6b4c8
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-01-23 21:08:11 -0600

    Se agregan capturas, script sql y readme

---
+++ EjercicioASPNetCoreSql.sql
@@ -0,0 +1,122 @@
+DROP DATABASE EjercicioAspNetCore;
+CREATE DATABASE EjercicioAspNetCore;
+
+GO
+
+USE EjercicioAspNetCore;
+
+GO
+
+
+
+CREATE TABLE Tiendas(
+	
+	idTienda UNIQUEIDENTIFIER PRIMARY KEY,
+	sucursal varchar(40),
+	codigoPostal VARCHAR(5),
+	estado varchar(50),	
+	idUsuario uniqueidentifier,
+	delegacionMunicipio varchar(60),
+	calleNum varchar(50)
+
+);
+
+CREATE TABLE Articulos(
+	
+	codigo UNIQUEIDENTIFIER PRIMARY KEY,
+	descripcion varchar(max),
+	precio money,
+	imagen varchar(max),
+	stock int,
+	fechaAlta datetime,
+	idUsuario uniqueidentifier
+
+);
+
+CREATE TABLE DetalleArticulosCompra(
+    idDetalleCompra UNIQUEIDENTIFIER PRIMARY KEY,
+	codigo UNIQUEIDENTIFIER ,
+	idCompra UNIQUEIDENTIFIER,
+	precio money,
+	cantidad int,
+	fechaCompra datetime
+
+);
+
+
+CREATE TABLE ArticulosTiendas(
+
+	idArticuloTienda uniqueidentifier PRIMARY KEY,
+	codigo uniqueidentifier,
+	idTienda uniqueidentifier,
+	fecha datetime
+
+);
+
+CREATE TABLE ClientesArticulosCompra(
+
+	idCompra uniqueidentifier PRIMARY KEY,
+	idUsuario uniqueidentifier,
+	fecha datetime,
+	total decimal
+
+);
+
+--Tabla muy sencilla por mótivos práscticos
+CREATE TABLE Usuarios(idUsuario uniqueidentifier PRIMARY KEY, correo varchar(70), contrasenia varchar(50),
+	nombre varchar(30),
+	codigoPostal VARCHAR(5),
+	estado varchar(50),
+	delegacionMunicipio varchar(60),
+	calleNum varchar(50), estaActivo bit);
+
+CREATE TABLE UsuarioRol(idRolUsuario uniqueidentifier PRIMARY KEY,idRol uniqueidentifier, idUsuario uniqueidentifier);
+
+CREATE TABLE Rol(IdRol UNIQUEIDENTIFIER PRIMARY KEY, nombre varchar(70), descripcion varchar(300) );
+
+--Creación de llaves foraneas
+
+ALTER TABLE ArticulosTiendas
+ADD FOREIGN KEY (codigo) REFERENCES Articulos(codigo);
+
+ALTER TABLE ArticulosTiendas
+ADD FOREIGN KEY (idTienda) REFERENCES Tiendas(idTienda);
+
+
+ALTER TABLE UsuarioRol
+ADD FOREIGN KEY (idRol) REFERENCES Rol(idRol);
+
+ALTER TABLE UsuarioRol
+ADD FOREIGN KEY (idUsuario) REFERENCES Usuarios(idUsuario);
+
+ALTER TABLE DetalleArticulosCompra
+ADD FOREIGN KEY (idCompra) REFERENCES ClientesArticulosCompra(idCompra);
+
+ALTER TABLE ClientesArticulosCompra
+ADD FOREIGN KEY (idUsuario) REFERENCES Usuarios(idUsuario);
+
+ALTER TABLE Tiendas
+ADD FOREIGN KEY (idUsuario) REFERENCES Usuarios(idUsuario);
+
+ALTER TABLE Articulos
+ADD FOREIGN KEY (idUsuario) REFERENCES Usuarios(idUsuario);
+
+
+
+------------------------------------------------------------------
+
+
+
+DECLARE @primerRol uniqueidentifier
+DECLARE @segundoRol uniqueidentifier
+
+
+SET @primerRol = NEWID()
+SET @segundoRol = NEWID()
+
+
+INSERT INTO Rol VALUES(@primerRol,'Vendedor','');
+INSERT INTO Rol VALUES(@segundoRol,'Cliente','');
+
+
+
---
+++ README.md
@@ -0,0 +1,27 @@
+# Ejercicio .Net Core 3.1
+
+Pasos a seguir para su funcionamiento
+
+1.-Ejecutar el archivo EjercicioASPNetCoreSql.sql para crear las tablas o restaurar el bd.bak
+
+2.-Cambiar conexión a bd en appsetings.json del proyecto mvc de presentación
+
+3.-Existen 2 tipos de usuarios: Vendedor y Cliente
+
+(Nota: Es importante tener esos dos roles insetador en la tabla Rol para el correcto funcioanamiento del proyecto)
+
+# Vistas de login y registro de usuario
+
+![cap1](https://github.com/AlfredoSV/Crud-Obreros-JavaSE/blob/main/imagenes/cap1.PNG)
+
+# Vistas para vendedor
+
+![cap2](https://github.com/AlfredoSV/Crud-Obreros-JavaSE/blob/main/imagenes/cap2.PNG)
+
+
+# Vistas para cliente 
+
+![cap3](https://github.com/AlfredoSV/Crud-Obreros-JavaSE/blob/main/imagenes/cap3.PNG)
+
+
+
